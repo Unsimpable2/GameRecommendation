75014d5c8ecdab7db3961435002e90cb99088cc6
Natalia Pstrag <nati.pstrag@gmail.com>
2024-10-14 19:13:33 +0200

    testy
    Former-commit-id: ace4bac0d93dda62bb36feb918eacc787bbd76e6

--- steam_database.py
+++ steam_database.py
@@ -7,7 +7,7 @@ response = requests.get(url)
 data = response.json()
 
 # Zapisz całą listę gier do pliku JSON
-with open('lista_gier_steam.json', 'w', encoding='utf-8') as file:
+with open('steam_game_list.json', 'w', encoding='utf-8') as file:
     json.dump(data['applist']['apps'], file, ensure_ascii=False, indent=4)
 
-print("Lista gier została zapisana do pliku lista_gier_steam.json")
+print("Lista gier została zapisana do pliku steam_game_list.json")
