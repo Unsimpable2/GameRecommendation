46ce4174637de743a508e20cee9de692a597a165
glimasalwi2 <witold.masal@valmet.com>
2024-10-15 12:18:50 +0200

    Ciag dalszy pobierania bazy + zmiana na ang
    Former-commit-id: a5dc1888306ea95fc61481648e27f1f8740aba12 [formerly a531a80a4cd83c6f82a9af94519145b4a33a824b]
    Former-commit-id: 3200904d5e4d71a17a5dd58df6001b049c216860

--- get_steam_database.py
+++ get_steam_database.py
@@ -6,8 +6,7 @@ url = 'http://api.steampowered.com/ISteamApps/GetAppList/v2/'
 response = requests.get(url)
 data = response.json()
 
-# Zapisz całą listę gier do pliku JSON
 with open('steam_game_list.json', 'w', encoding='utf-8') as file:
     json.dump(data['applist']['apps'], file, ensure_ascii=False, indent=4)
 
-print("Lista gier została zapisana do pliku steam_game_list.json")
+print("The game list has been saved to the steam_game_list.json file")
