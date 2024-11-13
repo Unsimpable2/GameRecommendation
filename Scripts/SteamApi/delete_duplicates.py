274daf2674810e904b993c98e4e3ab998096bc5b
glimasalwi2 <witold.masal@valmet.com>
2024-11-13 15:55:04 +0100

    Pobieranie danych, test pobieranie tagów gier

---
+++ Scripts/SteamApi/delete_duplicates.py
@@ -0,0 +1,30 @@
+import json
+
+def make_hashable(item):
+    if isinstance(item, dict):
+        return tuple((key, make_hashable(value)) for key, value in item.items())
+    elif isinstance(item, list):
+        return tuple(make_hashable(elem) for elem in item)
+    return item
+
+def delete_duplicates(file_name):
+    file_path = f'../GameRecommendation/Data/GamesData/{file_name}'
+    with open(file_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    unique_data = []
+    seen_sections = set()
+
+    for entry in data:
+        entry_tuple = make_hashable(entry)
+        
+        if entry_tuple not in seen_sections:
+            seen_sections.add(entry_tuple)
+            unique_data.append(entry)
+
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(unique_data, f, indent=4, ensure_ascii=False)
+
+    print("Duplicates have been removed.")
+
+delete_duplicates('duplicates.json')
