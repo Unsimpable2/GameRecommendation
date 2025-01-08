e98a47daf5f8a0542cabcd0c4f3a8b9e1bd9af35
glimasalwi2 <witold.masal@valmet.com>
2025-01-08 14:02:43 +0100

    Nowa nazwa update_database + zmiany właczania go
    Former-commit-id: 441328fc2678af6ef3e425e4203aeab007e5116b [formerly 171e3a7a2bb675dba76ff9cd9f17a2aedce6f396]
    Former-commit-id: b0e3a9e5f578622812e06c9976666631a2b24c48

--- Scripts/SteamApi/update_game_list.py
+++ Scripts/SteamApi/update_game_list.py
@@ -13,11 +13,16 @@ new_file_path = base_path + data_list_path + '/BaseList/steam_game_list_new.json
 update_file_path = base_path + data_list_path + '/DownloadList/steam_game_list_to_update.json'
 last_update_file_path = base_path + log_update_path + '/last_database_update.txt'
 
-logging.basicConfig(
-    filename = base_path + log_update_path + '/steam_game_updater.log',
-    level = logging.INFO,
-    format = '%(asctime)s - %(levelname)s - %(message)s'
-)
+def get_logger():
+    logger = logging.getLogger("steam_game_updater")
+    logger.setLevel(logging.INFO)
+    log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler(base_path + log_update_path + '/steam_game_updater.log')
+    file_handler.setFormatter(log_formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+logger = get_logger()
 
 def fetch_steam_game_data():
     url = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/'
@@ -26,13 +31,13 @@ def fetch_steam_game_data():
     if response.status_code == 200:
         return [{"appid": game["appid"], "name": game["name"]} for game in response.json()['applist']['apps']]
     else:
-        logging.error(f"Error fetching data from Steam API: {response.status_code}")
+        logger.error(f"Error fetching data from Steam API: {response.status_code}")
         raise Exception(f"Error fetching data from Steam API: {response.status_code}")
 
 def save_to_json(data, file_path):
     with open(file_path, 'w', encoding = 'utf-8') as json_file:
         json.dump(data, json_file, indent = 4, ensure_ascii = False)
-    logging.info(f"Data saved to {file_path}")
+    logger.info(f"Data saved to {file_path}")
 
 def compare_game_lists(base_data, new_data):
     base_ids = {game['appid'] for game in base_data}
@@ -47,39 +52,39 @@ def delete_duplicates():
     file_path = update_file_path
 
     try:
-        with open(file_path, 'r', encoding = 'utf-8') as f:
+        with open(file_path, 'r', encoding='utf-8') as f:
             data = json.load(f)
-            logging.info(f"Read {len(data)} entries from the file before removing duplicates.")
+            logger.info(f"Read {len(data)} entries from the file before removing duplicates.")
 
         if not data:
-            logging.warning("The file is empty. Skipping duplicate removal.")
+            logger.warning("The file is empty. Skipping duplicate removal.")
             return
 
         seen = set()
         unique_data = []
         for entry in data:
-            entry_str = json.dumps(entry, sort_keys = True)
+            entry_str = json.dumps(entry, sort_keys=True)
             if entry_str not in seen:
                 seen.add(entry_str)
                 unique_data.append(entry)
 
         if len(unique_data) == len(data):
-            logging.info("No duplicates found in the file.")
+            logger.info("No duplicates found in the file.")
         else:
-            logging.info("Duplicates have been removed.")
+            logger.info("Duplicates have been removed.")
 
         with open(file_path, 'w', encoding = 'utf-8') as f:
             json.dump(unique_data, f, indent = 4, ensure_ascii = False)
-            logging.info(f"Unique data saved: {unique_data}")
+            logger.info(f"Unique data saved: {unique_data}")
 
     except json.JSONDecodeError:
-        logging.error(f"File {file_path} is not a valid JSON. Skipping duplicate removal.")
+        logger.error(f"File {file_path} is not a valid JSON. Skipping duplicate removal.")
     except Exception as e:
-        logging.error(f"Error during duplicate removal: {e}")
+        logger.error(f"Error during duplicate removal: {e}")
         raise
 
-def main():
-    logging.info("Starting the update process.")
+def update_game_list():
+    logger.info("Starting the update process.")
     try:
         new_data = fetch_steam_game_data()
         save_to_json(new_data, new_file_path)
@@ -88,7 +93,7 @@ def main():
             with open(base_file_path, 'r', encoding = 'utf-8') as base_file:
                 base_data = json.load(base_file)
         else:
-            logging.warning("No existing game database found, creating a new one.")
+            logger.warning("No existing game database found, creating a new one.")
             base_data = []
 
         if os.path.exists(update_file_path):
@@ -103,7 +108,7 @@ def main():
 
         if not missing_games:
             print("The game database is up to date. No missing games found.")
-            logging.info("No missing games found. Database is up to date.")
+            logger.info("No missing games found. Database is up to date.")
             if os.path.exists(new_file_path):
                 os.remove(new_file_path)
             return
@@ -113,9 +118,9 @@ def main():
 
         if os.path.exists(base_file_path):
             os.remove(base_file_path)
-            logging.info(f"Removed old base file: {base_file_path}")
+            logger.info(f"Removed old base file: {base_file_path}")
         os.rename(new_file_path, base_file_path)
-        logging.info(f"Updated base file: {base_file_path}")
+        logger.info(f"Updated base file: {base_file_path}")
 
         delete_duplicates()
 
@@ -127,13 +132,10 @@ def main():
             last_update_file.write(f"Elements Added: {num_added_elements}\n")
             last_update_file.write("------------End of update------------\n\n")
 
-        logging.info("Updated timestamp of the last database update with new games information.")
-        logging.info("------------End of update------------\n")
+        logger.info("Updated timestamp of the last database update with new games information.")
+        logger.info("------------End of update------------\n")
         print("Game database update completed successfully.")
 
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}")
         print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    main()
